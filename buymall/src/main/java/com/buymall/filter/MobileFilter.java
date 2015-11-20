bd842d53cad2345d220b2a4d9da6ee86592a0e4c
zhoudong <11230595@qq.com>
2015-11-20 14:06:01 +0800

    filter修改

--- buymall/src/main/java/com/buymall/filter/MobileFilter.java
+++ buymall/src/main/java/com/buymall/filter/MobileFilter.java
@@ -1,6 +1,7 @@
 package com.buymall.filter;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,28 +12,41 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.buymall.constants.Constants;
+import com.buymall.view.MemberController;
 /**
  * 判断是否是手机登录
  * @author zhoudong
  *
  */
 public class MobileFilter implements Filter{
-
+	private static Logger logger = Logger.getLogger(MobileFilter.class);
 	public void destroy() {
 		
 	}
 
-	public void doFilter(ServletRequest request, ServletResponse response,
+	public void doFilter(ServletRequest req, ServletResponse resp,
 			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest request2 = (HttpServletRequest) request;
-		HttpServletResponse httpRes = (HttpServletResponse)response;
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse)resp;
+		String uri = request.getRequestURI();
+		logger.info("filter拦截访问的URI---》" + uri);
 		
-		String userAgent = request2.getHeader("user-agent");  //请求从哪来的
+		String userAgent = request.getHeader("user-agent");  //请求从哪来的
 		if(userAgent.contains("Android") || userAgent.contains("iPhone")){
-			httpRes.sendRedirect(Constants.config.getString("MOBILE_URL"));
+			List<String> uris = Constants.config.getList("GO_URI");
+			for(String goUri : uris){
+				if(uri.contains(goUri)){
+					logger.info("filter --》 手机访问参数在放行列表中，放行。--》" + uri);
+					chain.doFilter(req, resp);
+					return;
+				}
+			}
+			response.sendRedirect(Constants.config.getString("MOBILE_URL"));
 		}else {
-			chain.doFilter(request, response);  
+			chain.doFilter(req, resp);  
 		}
 	}
 
