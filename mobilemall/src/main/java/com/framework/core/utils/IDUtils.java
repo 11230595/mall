2f4c2ad5a633a3abb96f4e19ff7c4d6936c3c71b
zhoudong <11230595@qq.com>
2015-11-23 14:47:56 +0800

    ID

--- mobilemall/src/main/java/com/framework/core/utils/IDUtils.java
+++ mobilemall/src/main/java/com/framework/core/utils/IDUtils.java
@@ -1,99 +1,77 @@
 package com.framework.core.utils;
+
+import java.text.DecimalFormat;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Logger;
+
 /**
  * ID及随机数处理类
  * @author zhoudong
  *
  */
 public class IDUtils {
-	private final static long twepoch = 1288834974657L;
-	// 机器标识位数
-	private final static long workerIdBits = 5L;
-	// 数据中心标识位数
-	private final static long datacenterIdBits = 5L;
-	// 机器ID最大值
-	private final static long maxWorkerId = -1L ^ (-1L << workerIdBits);
-	// 数据中心ID最大值
-	private final static long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
-	// 毫秒内自增位
-	private final static long sequenceBits = 12L;
-	// 机器ID偏左移12位
-	private final static long workerIdShift = sequenceBits;
-	// 数据中心ID左移17位
-	private final static long datacenterIdShift = sequenceBits + workerIdBits;
-	// 时间毫秒左移22位
-	private final static long timestampLeftShift = sequenceBits + workerIdBits
-			+ datacenterIdBits;
-
-	private final static long sequenceMask = -1L ^ (-1L << sequenceBits);
-
-	private static long lastTimestamp = -1L;
-
-	private long sequence = 0L;
-	private final long workerId;
-	private final long datacenterId;
-
-	public IDUtils(long workerId, long datacenterId) {
-		if (workerId > maxWorkerId || workerId < 0) {
-			throw new IllegalArgumentException(
-					"worker Id can't be greater than %d or less than 0");
-		}
-		if (datacenterId > maxDatacenterId || datacenterId < 0) {
-			throw new IllegalArgumentException(
-					"datacenter Id can't be greater than %d or less than 0");
-		}
-		this.workerId = workerId;
-		this.datacenterId = datacenterId;
-	}
-
-	public synchronized long nextId() {
-		long timestamp = timeGen();
-		if (timestamp < lastTimestamp) {
-			try {
-				throw new Exception(
-						"Clock moved backwards.  Refusing to generate id for "
-								+ (lastTimestamp - timestamp) + " milliseconds");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		if (lastTimestamp == timestamp) {
-			// 当前毫秒内，则+1
-			sequence = (sequence + 1) & sequenceMask;
-			if (sequence == 0) {
-				// 当前毫秒内计数满了，则等待下一秒
-				timestamp = tilNextMillis(lastTimestamp);
-			}
-		} else {
-			sequence = 0;
-		}
-		lastTimestamp = timestamp;
-		// ID偏移组合生成最终的ID，并返回ID
-		long nextId = ((timestamp - twepoch) << timestampLeftShift)
-				| (datacenterId << datacenterIdShift)
-				| (workerId << workerIdShift) | sequence;
-
-		return nextId;
-	}
-
-	private long tilNextMillis(final long lastTimestamp) {
-		long timestamp = this.timeGen();
-		while (timestamp <= lastTimestamp) {
-			timestamp = this.timeGen();
-		}
-		return timestamp;
-	}
-
-	private long timeGen() {
-		return System.currentTimeMillis();
-	}
+	/** .log */
+    private static final Logger logger = Logger.getLogger(IDUtils.class);
+ 
+    /** The FieldPosition. */
+    private static final FieldPosition HELPER_POSITION = new FieldPosition(0);
+ 
+    /** This Format for format the data to special format. */
+    private final static Format dateFormat = new SimpleDateFormat("MMddHHmmssS");
+ 
+    /** This Format for format the number to special format. */
+    private final static NumberFormat numberFormat = new DecimalFormat("0000");
+ 
+    /** This int is the sequence number ,the default value is 0. */
+    private static int seq = 0;
+ 
+    private static final int MAX = 9999;
+ 
+    /**
+     * 时间格式生成序列
+     * @return String
+     */
+    public static synchronized String generateSequenceNo() {
+ 
+        Calendar rightNow = Calendar.getInstance();
+ 
+        StringBuffer sb = new StringBuffer();
+ 
+        dateFormat.format(rightNow.getTime(), sb, HELPER_POSITION);
+ 
+        numberFormat.format(seq, sb, HELPER_POSITION);
+ 
+        if (seq == MAX) {
+            seq = 0;
+        } else {
+            seq++;
+        }
+ 
+        logger.info("THE SQUENCE IS :" + sb.toString());
+ 
+        return sb.toString();
+    }
 
 	/**
 	 * 生成ID
 	 * @return
 	 */
-	public static String getId(){
-		return String.valueOf(new IDUtils(1,2).nextId());
+	public static String getId() {
+		return generateSequenceNo();
+	}
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 50; i++) {
+			System.out.println(getId());
+		}
 	}
 	
 	/**
@@ -102,6 +80,6 @@ public class IDUtils {
 	 * @return
 	 */
 	public static int getRandom() {
-		return (int) (Math.random() * 3);
+		return (int) (Math.random() * 300);
 	}
 }
