bde2af434c4e1884ed7c777578684dc769fe1954
zhoudong <11230595@qq.com>
2015-12-06 11:13:53 +0800

    1

--- buymall/src/main/java/com/buymall/utils/TypeUtils.java
+++ buymall/src/main/java/com/buymall/utils/TypeUtils.java
@@ -28,28 +28,44 @@ public class TypeUtils {
 			}
 		}
 		
-		if(type == 12){ //九块九专区
-			if(title.contains("外套") || title.contains("衬衫") || title.contains("T恤") ||  title.contains("裤子")
+		if(type == 12 || type == 7){ //九块九专区
+			if(title.contains("外套") || title.contains("衬衫") || title.contains("T恤") ||  title.contains("裤子") 
 					|| title.contains("羽绒服") || title.contains("毛衫") || title.contains("卫衣") ||title.contains("牛仔裤")
-					||	title.contains("休闲裤")){
+					||	title.contains("休闲裤")|| title.contains("长袖")){
 				return getType(title,1);
 			}
-			if(title.contains("女装") || title.contains("打底裤")){
+			if(title.contains("女装") || title.contains("打底裤")  ||	title.contains("裙")|| title.contains("吊带")){
 				return 1;
 			}
 			if(title.contains("男装")){
 				return 2;
 			}
-			if(title.contains("内衣")){
+			if(title.contains("内衣") || title.contains("胸罩") || title.contains("裤头") 
+					|| title.contains("内裤") || title.contains("袜")){
 				return 3;
 			}
 			if(title.contains("鞋")){
 				return 4;
 			}
-			if(title.contains("吃")){
+			if(title.contains("吃") || title.contains("食") || title.contains("水果") || title.contains("罐头")
+					|| title.contains("枣") || title.contains("枸杞")|| title.contains("茶") || title.contains("普洱")
+					|| title.contains("干货") || title.contains("木耳") || title.contains("蘑菇") || title.contains("蜂蜜")){
 				return 8;
 			}
+			if(title.contains("壳") || title.contains("膜") || title.contains("耳机") || title.contains("电源")){
+				return 9;
+			}
 			if(title.contains("包") || title.contains("箱")){
+				return 10;
+			}
+			
+			if(title.contains("美甲") || title.contains("香水") || title.contains("指甲") || title.contains("皮筋")
+					|| title.contains("饰品") || title.contains("发卡") || title.contains("耳环") || title.contains("耳钉")
+					|| title.contains("香水") || title.contains("指环") || title.contains("戒指") || title.contains("洗发水")
+					|| title.contains("洗面奶") || title.contains("口红") || title.contains("洁面乳") || title.contains("胭脂")
+					|| title.contains("眼线") || title.contains("妆") || title.contains("假发") || title.contains("面部")
+					|| title.contains("乳液") || title.contains("润肤乳")|| title.contains("美白") || title.contains("唇膏")
+					|| title.contains("香脂")){
 				return 10;
 			}
 		}
--- buymall/src/main/webapp/buymall/index.ftl
+++ buymall/src/main/webapp/buymall/index.ftl
@@ -75,7 +75,7 @@
 												<#if page.title?length gt 46>${page.title?substring(0,46)}...<#else>${page.title}</#if>
 											</h3>
 											<h4>
-												￥<span style="font-size:28px;font-weight:bold;">${page.zkFinalPrice!'19.00'}</span>&nbsp;&nbsp;
+												￥<span style="font-size:28px;font-weight:bold;">${page.zkFinalPrice!'19.00'}</span>&nbsp;
 												<span style="color:#999999;">${page.sale!''}折</spa>
 												<#if user?? && user.userLevel == 8><!-- 如果是管理员，显示删除选项 -->
 													<span style="color:red;">
