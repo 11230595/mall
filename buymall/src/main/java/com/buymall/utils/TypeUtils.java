b6aeb94efa40eaa18570db0c5887f50ed467147a
zhoudong <11230595@qq.com>
2015-12-21 10:43:14 +0800

    type

--- buymall/src/main/java/com/buymall/utils/TypeUtils.java
+++ buymall/src/main/java/com/buymall/utils/TypeUtils.java
@@ -5,6 +5,12 @@ package com.buymall.utils;
  *
  */
 public class TypeUtils {
+	
+	private static String[] yifus = {"外套","衬衫","T恤","裤","羽绒服","毛衫","卫衣","牛仔裤","休闲裤","长袖","修身","显瘦","袄","哈伦","马甲"};
+	private static String[] xiaochis = {"吃","食","水果","罐头","枣","枸杞","茶","普洱","干货","木耳","蘑菇","蜂蜜","辣椒","大米","黄小米"};
+	private static String[] meizhuangs = {"美甲","香水","指甲","皮筋","饰品","发卡","耳环","耳钉","香水","指环","戒指","洗发水","洗面奶","口红",
+		"洁面乳","胭脂","眼线","妆","假发","面部","乳液","润肤乳","美白","唇膏","香脂"};
+	
 	/**
 	 * 获取类型
 	 * @param title 标题
@@ -18,6 +24,7 @@ public class TypeUtils {
 			}else if (title.contains("男")) {
 				return 2;
 			}
+			return type;
 		}
 		
 		if(type == 4 || type == 10){
@@ -26,14 +33,25 @@ public class TypeUtils {
 			}else if (title.contains("包") || title.contains("箱")) {
 				return 10;
 			}
+			return type;
 		}
-		
 		if(type == 12 || type == 7){ //九块九专区
-			if(title.contains("外套") || title.contains("衬衫") || title.contains("T恤") ||  title.contains("裤子") 
-					|| title.contains("羽绒服") || title.contains("毛衫") || title.contains("卫衣") ||title.contains("牛仔裤")
-					||	title.contains("休闲裤")|| title.contains("长袖")){
-				return getType(title,1);
+			//衣服
+			for(String yifu : yifus){
+				if(title.contains(yifu))
+					return getType(title,1);
 			}
+			//美食
+			for(String xiaochi : xiaochis){
+				if(title.contains(xiaochi))
+					return 8;
+			}
+			//美妆
+			for(String meizhuang : meizhuangs){
+				if(title.contains(meizhuang))
+					return 11;
+			}
+			
 			if(title.contains("女装") || title.contains("打底裤")  ||	title.contains("裙")|| title.contains("吊带")){
 				return 1;
 			}
@@ -47,25 +65,11 @@ public class TypeUtils {
 			if(title.contains("鞋")){
 				return 4;
 			}
-			if(title.contains("吃") || title.contains("食") || title.contains("水果") || title.contains("罐头")
-					|| title.contains("枣") || title.contains("枸杞")|| title.contains("茶") || title.contains("普洱")
-					|| title.contains("干货") || title.contains("木耳") || title.contains("蘑菇") || title.contains("蜂蜜")){
-				return 8;
-			}
+			
 			if(title.contains("壳") || title.contains("膜") || title.contains("耳机") || title.contains("电源")){
 				return 9;
 			}
 			if(title.contains("包") || title.contains("箱")){
-				return 10;
-			}
-			
-			if(title.contains("美甲") || title.contains("香水") || title.contains("指甲") || title.contains("皮筋")
-					|| title.contains("饰品") || title.contains("发卡") || title.contains("耳环") || title.contains("耳钉")
-					|| title.contains("香水") || title.contains("指环") || title.contains("戒指") || title.contains("洗发水")
-					|| title.contains("洗面奶") || title.contains("口红") || title.contains("洁面乳") || title.contains("胭脂")
-					|| title.contains("眼线") || title.contains("妆") || title.contains("假发") || title.contains("面部")
-					|| title.contains("乳液") || title.contains("润肤乳")|| title.contains("美白") || title.contains("唇膏")
-					|| title.contains("香脂")){
 				return 10;
 			}
 		}
